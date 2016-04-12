df6f4782979397543282919967e36ca9ee96b9bc
Kevin Quigley <kevinquigley@Kevins-MacBook-Air.local>
2016-04-12 16:32:42 -0400

    fixed birdie asset

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,5 @@
 Tiki.create([
   { name: "Angry Base", thumbnail_path: "nil.png", object_path: "assets/angry_base.x3d", height: 3.08445 },
   { name: "Stoned Base", thumbnail_path: "nil.png", object_path: "assets/stoned_base.x3d", height: 3.12001},
-  { name: "Squinty Birdie", thumbnail_path: "nil.png", object_path: "assets/squinty_birdie.x3d", height: 2.03743}
+  { name: "Squinty Birdie", thumbnail_path: "nil.png", object_path: "assets/squinty_birdie_best.x3d", height: 2.03743}
   ])
